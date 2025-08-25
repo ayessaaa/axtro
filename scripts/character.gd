91ad3450ec69507c23f3fc477759723cb55b07c7
Ayessa <ayessamaedeguzman78@gmail.com>
2025-08-25 15:06:00 +0800

    player animation working wooooo

--- scripts/character.gd
+++ scripts/character.gd
@@ -4,7 +4,8 @@ extends Area2D
 @export var player = true
 var screen_size
 
-@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
 
 const METEOR = preload("res://scenes/meteor.tscn")
 @onready var meteors_container = get_parent().get_node("Meteors")
@@ -22,9 +23,17 @@ func _ready() -> void:
 	spawn_meteor(Vector2(1800, randf_range(50, screen_size[1]-100)))
 	spawn_meteor(Vector2(2000, randf_range(50, screen_size[1]-100)))
 	
+var down_sub_counter = 0
+var down_sub_counter2 = 0
+
+var up_sub_counter = 0
+var up_sub_counter2 = 0
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var velocity = Vector2.ZERO # The player's movement vector.
+	
 		
 	if Input.is_action_pressed("move_right") and position.x <= 1050:
 		velocity.x += 1
@@ -32,8 +41,45 @@ func _process(delta: float) -> void:
 		velocity.x -= 1
 	if Input.is_action_pressed("move_down") and position.y <= 520:
 		velocity.y += 1
+		if Input.is_action_just_pressed("move_down"):
+			animated_sprite_2d.play("down_sub")
+		else:
+			if down_sub_counter >= 1:
+				animated_sprite_2d.play("down")
+			else:
+				down_sub_counter += delta * 10
+
 	if Input.is_action_pressed("move_up") and position.y >= 70:
 		velocity.y -= 1
+		if Input.is_action_just_pressed("move_up"):
+			animated_sprite_2d.play("up_sub")
+		else:
+			if up_sub_counter >= 1:
+				animated_sprite_2d.play("up")
+			else:
+				up_sub_counter += delta * 10
+		
+	if Input.is_action_pressed("move_down") or Input.is_action_pressed("move_up"):
+		pass
+	else:
+		
+		if down_sub_counter > 0:
+			animated_sprite_2d.play("down_sub")
+			down_sub_counter2 += delta * 10
+			print(down_sub_counter)
+			if down_sub_counter2 >= 1:
+				animated_sprite_2d.play("default")
+				down_sub_counter = 0
+				down_sub_counter2 = 0
+		if up_sub_counter > 0:
+			animated_sprite_2d.play("up_sub")
+			up_sub_counter2 += delta * 10
+			print(up_sub_counter)
+			if up_sub_counter2 >= 1:
+				animated_sprite_2d.play("default")
+				up_sub_counter = 0
+				up_sub_counter2 = 0
+		
 		
 	if Input.is_action_just_pressed("shoot"):
 		shoot()
