a6592c7aa3fb989b652bd28e24f03d46a3f28e68
Ayessa <ayessamaedeguzman78@gmail.com>
2025-09-07 16:20:10 +0800

    ufo assets

--- scripts/space_ray.gd
+++ scripts/space_ray.gd
@@ -2,6 +2,7 @@ extends Node
 @onready var bg: Sprite2D = $Bg
 
 @onready var selected_sound = get_parent().get_node("SelectedSound")
+@onready var animation = get_node("SpaceRayCharacter/AnimationPlayer")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,4 +14,5 @@ func _process(delta: float) -> void:
 	bg.visible = Global.is_space_ray
 	if !Global.selected_sound_played and Global.is_space_ray:
 		selected_sound.play()
+		animation.play("fade_in")
 		Global.selected_sound_played = true
